337a7cc7afe6f99fe840c28103c9f16c72d02ac1
Sanath-here <sanath.lal2023@gmail.com>
2026-05-21 23:55:48 +0530

    final commit

---
+++ android/app/src/main/java/com/example/energogame/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.energogame;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.energogame',
+  appName: 'energo-game',
+  webDir: 'dist'
+};
+
+export default config;
